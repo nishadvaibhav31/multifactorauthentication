5ab372126a1483d09bafea02ff646a06d2ccc085
nishadvaibhav31 <135223842+nishadvaibhav31@users.noreply.github.com>
2025-03-05 19:13:41 +0530

    Update authcontroller.js

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -133,8 +133,9 @@ const {username}=req.body;
   res.clearCookie("auth_token");  
  
 
-  res.status(200).json({message:"user deleted successfully"});
+  return res.status(200).json({message:"user deleted successfully"});
   }
   catch(error){
+  retunr res.status(400).json({message:"error in deleting user"});
 }
-}
+};
