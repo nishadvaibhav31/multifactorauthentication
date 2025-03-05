0cf1ba2153f7b0861f30d64e30b12bd6fae08502
nishadvaibhav31 <135223842+nishadvaibhav31@users.noreply.github.com>
2025-03-05 13:52:50 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -118,7 +118,7 @@ function Signup() {
             setUser(null)
             localStorage.removeItem("user")
             setotp(null)
-            deleteuser(username)
+           await  deleteuser(username)
           }
              }
             className={`${!otp?'hidden':''} w-full p-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500`}
