6c665560ffc42ca29148cbad4f503158f4cff362
nishadvaibhav31 <135223842+nishadvaibhav31@users.noreply.github.com>
2025-07-30 19:57:19 +0530

    Update verifytoken.js

--- backend/middleware/verifytoken.js
+++ backend/middleware/verifytoken.js
@@ -1,7 +1,9 @@
 import jwt from "jsonwebtoken";
 import User from "../models/users.model.js"; 
+import redisClient from "../config/redis.js"; 
 
 export const tokenverification = async (req, res, next) => {
+  console.log(req.cookies);
   const token = req.cookies.auth_token;
 
   if (!token) {
@@ -9,22 +11,32 @@ export const tokenverification = async (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-
-    if (!decoded || !decoded.userId) {
-      return res.status(403).json({ message: "Invalid token payload." });
-    }
-
-    const user = await User.findById(decoded.userId).select("-password");
     
-    if (!user) {
-      return res.status(401).json({ message: "User not found." });
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const userId = decoded.userId; 
+    const userCacheKey = `user:${userId}`;
+    
+    const cachedUser = await redisClient.get(userCacheKey);
+
+    if (cachedUser) {
+
+      req.user = JSON.parse(cachedUser);
+    } else {
+      
+      const user = await User.findById(userId).select("-password"); 
+      if (!user) {
+        return res.status(401).json({ message: "User not found." });
+      }
+
+      req.user = user;
+
+      await redisClient.set(userCacheKey, JSON.stringify(user), "EX", 900);
     }
 
-    req.user = user;
+
     next();
   } catch (err) {
-    console.error("JWT verification failed:", err);
+    
     return res.status(403).json({ message: "Forbidden: Invalid token." });
   }
-};
+}; remove redis 
