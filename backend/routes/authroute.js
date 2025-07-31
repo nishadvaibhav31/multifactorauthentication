19ab95e4bce97c89341c9bb181a2ab4d81aab45c
nishadvaibhav31 <135223842+nishadvaibhav31@users.noreply.github.com>
2025-07-31 16:24:24 +0530

    Update authroute.js

--- backend/routes/authroute.js
+++ backend/routes/authroute.js
@@ -1,14 +1,12 @@
 import {Router} from "express";
-import {login,signup,logout,change, reset,deleteuser} from "../controllers/authcontroller.js"
+import {login,signup,logout, reset} from "../controllers/authcontroller.js"
 import {tokenverification} from "../middleware/verifytoken.js"
 
 const router= Router();
 router.post("/login",login);
 router.post("/signup",signup);
 router.post("/logout",logout);
-router.post("/change",tokenverification,change);
 router.post("/reset",reset);
-router.post("/delete",deleteuser);
 
 
 export default router;
